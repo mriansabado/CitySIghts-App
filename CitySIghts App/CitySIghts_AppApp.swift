6828dbc8d8bc0715848aba16ccf1c23c7c33bc72
mriansabado <mriansabado@gmail.com>
2024-10-12 22:36:16 -0700

    Initial Commit

---
+++ CitySIghts App/CitySIghts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySIghts_AppApp.swift
+//  CitySIghts App
+//
+//  Created by Onpoint on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySIghts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
